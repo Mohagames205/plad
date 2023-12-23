a24485ca5bf1d7ca9939fe6c83b79ae20e263f8f
Mohamed <mohamedelyousfi87@gmail.com>
2023-12-23 19:11:50 +0100

    Add relation in EventComment model

--- app/Models/EventComment.php
+++ app/Models/EventComment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EventComment extends Model
 {
@@ -11,5 +12,9 @@ class EventComment extends Model
 
     protected $guarded = [];
 
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(CollectionEvent::class);
+    }
 
 }
