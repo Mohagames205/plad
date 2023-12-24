ed80abc648db90bef09dba42e03241e41563741d
Mohamed <mohamedelyousfi87@gmail.com>
2023-12-24 15:47:18 +0100

    Add some basic stats to the dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,18 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <div class="flex gap-6">
+                        <div class="shadow-sm p-6 border border-gray-200 rounded-lg basis-1/3">
+                            <h3 class="text-2xl tracking-tight text-gray-800 text-center">Totaal verkocht</h3>
+                            <p class="text-gray-800 text-lg mt-4">{{  \App\Models\CollectionEvent::sum('bandage_count') - \App\Models\EventComment::sum('remaining_bandages') }}</p>
+                        </div>
+
+                        <div class="shadow-sm p-6 border border-gray-200 rounded-lg basis-1/3">
+                            <h3 class="text-2xl tracking-tight text-gray-800 text-center">Totale winst</h3>
+                            <p class="text-gray-800 text-lg mt-4">€{{ \App\Models\EventComment::sum('money_after_event') - \App\Models\CollectionEvent::sum('change_received') }}</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
