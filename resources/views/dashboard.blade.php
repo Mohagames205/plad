7a35f214556c908327b8b5f23c830a9ad00ac3cd
Mohamed <mohamedelyousfi87@gmail.com>
2024-04-10 00:06:14 +0200

    Grafiek voor de funsies toegevoegd

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,8 +21,48 @@
                         </div>
                     </div>
 
+                    <div style="width: 800px;"><canvas id="acquisitions"></canvas></div>
+
                 </div>
             </div>
         </div>
     </div>
+
+    <script defer>
+
+
+
+
+
+        setTimeout(() => {
+            (async function() {
+                const data = [
+                    { year: 2010, count: 10 },
+                    { year: 2011, count: 20 },
+                    { year: 2012, count: 15 },
+                    { year: 2013, count: 25 },
+                    { year: 2014, count: 22 },
+                    { year: 2015, count: 30 },
+                    { year: 2016, count: 28 },
+                ];
+
+                new Chart(
+                    document.getElementById('acquisitions'),
+                    {
+                        type: 'bar',
+                        data: {
+                            labels: data.map(row => row.year),
+                            datasets: [
+                                {
+                                    label: 'Acquisitions by year',
+                                    data: data.map(row => row.count)
+                                }
+                            ]
+                        }
+                    }
+                );
+            })();
+        }, 100);
+
+    </script>
 </x-app-layout>
